1b97cb21c69d6176bb7e7a346091ee44cdd21bb8
Bruno Sesso <bruno.sesso@gmail.com>
2020-10-12 13:32:51 -0300

    User entity and its resource

---
+++ src/main/java/br/com/brunosesso/workshopspring/entities/User.java
@@ -0,0 +1,20 @@
+package br.com.brunosesso.workshopspring.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private String password;
+}
---
+++ src/main/java/br/com/brunosesso/workshopspring/resources/UserResources.java
@@ -0,0 +1,19 @@
+package br.com.brunosesso.workshopspring.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.brunosesso.workshopspring.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
