4c22f2d5ad96655ebf1430357014c018b5346702
Bruno Sesso <bruno.sesso@gmail.com>
2020-10-12 12:59:19 -0300

    Project created

---
+++ src/test/java/br/com/brunosesso/workshopspring/WorkshopspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunosesso.workshopspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
