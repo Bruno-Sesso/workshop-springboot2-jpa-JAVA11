5b14f03d4bfba0bb05906efae6544ad63ca20a4d
Bruno Sesso <bruno.sesso@gmail.com>
2020-10-12 16:27:34 -0300

    Enum OrderStatus

--- src/main/java/br/com/brunosesso/workshopspring/config/TestConfig.java
+++ src/main/java/br/com/brunosesso/workshopspring/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.brunosesso.workshopspring.entities.Order;
 import br.com.brunosesso.workshopspring.entities.User;
+import br.com.brunosesso.workshopspring.entities.enums.OrderStatus;
 import br.com.brunosesso.workshopspring.repositories.OrderRepository;
 import br.com.brunosesso.workshopspring.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456", null);
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456", null);
 		
-		Order o1 = new Order(null, Instant.parse("2020-09-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2020-10-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2020-10-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2020-09-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2020-10-21T03:42:10Z"), OrderStatus.WATTING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2020-10-22T15:21:22Z"), OrderStatus.WATTING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2)); 
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3)); 
