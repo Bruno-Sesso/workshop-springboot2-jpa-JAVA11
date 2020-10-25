2d0f07e12ded08dbeb15d50bb9fb71f2cc07a98e
Bruno Sesso <bruno.sesso@gmail.com>
2020-10-25 14:22:41 -0300

    Payment, one-to-one association

--- src/main/java/br/com/brunosesso/workshopspring/config/TestConfig.java
+++ src/main/java/br/com/brunosesso/workshopspring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.brunosesso.workshopspring.entities.Category;
 import br.com.brunosesso.workshopspring.entities.Order;
 import br.com.brunosesso.workshopspring.entities.OrderItem;
+import br.com.brunosesso.workshopspring.entities.Payment;
 import br.com.brunosesso.workshopspring.entities.Product;
 import br.com.brunosesso.workshopspring.entities.User;
 import br.com.brunosesso.workshopspring.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2020-09-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
